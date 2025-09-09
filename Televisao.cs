1de6e42eedb7b8a53f7fbad2eed8e684a73eacdb
cvzeraa <165086449+cvzeraa@users.noreply.github.com>
2025-09-09 00:01:00 +0000

    Feat: atividade

--- Televisao.cs
+++ Televisao.cs
@@ -1,21 +1,92 @@
 public class Televisao
 {
-    public Televisao(float tamanho){
-        Tamanho = tamanho;
-    }
-
+    
     public float Tamanho { get; set; }
     public int Resoluçao { get; set; }
     public int Volume { get; set; }
     public int Canal { get; set; }
     public bool Estado { get; set; }
+    private bool Mudo {get; set; }
+    private int UltimoCanal {get; set; }
 
-    public void AumentarVolume(){
-        Volume = Volume + 1;
+    //TV
+    private const int VolumeMinimo = 0;
+    private const int VolumeMaximo = 100;
+    private const int CanalMinimo = 1;
+    private const int CanalMaximo = 520;
+
+
+    public Televisao(float tamanho){
+        Tamanho = tamanho;
+        Volume = 50;
+        Canal = 1;
+        UltimoCanal = 1;
+        Estado = false;
+        Mudo = false;
     }
 
-    public void DiminuirVolume(){
-        Volume = Volume - 1;
+    //ESTADO
+    public void Ligar() {
+        Estado = true;
+        Canal = UltimoCanal;
+        Console.WriteLine($"TV ligada no canal {Canal}");
+    }
+
+    public void Desligar(){
+        Estado = false;
+        UltimoCanal = Canal;
+        Console.WriteLine("TV desligada");
+    }
+
+    //VOLUME
+    public void AumentarVolume(){
+        if(Volume < VolumeMaximo)
+    {
+        Volume++;
+        Console.WriteLine($"Volume: {Volume}");
+    }
+    else
+        {
+        Console.WriteLine("Volume já está no máximo");
         }
+    }
+    public void DiminuirVolume(){
+        if(Volume > VolumeMinimo)
+    {
+        Volume--;
+        Console.WriteLine($"Volume: {Volume}");
+    }
+    else
+        {
+            Console.WriteLine("Volume já está no mínimo");
+        }
+    }
+
+    public void AtivarMudo(){
+        Mudo = !Mudo;
+        if(Mudo)
+            Console.WriteLine("Mudo ativado");
+        else   
+            Console.WriteLine("Mudo Desativado. Volume atual: {Volume}");
+    }
+
+   //CANAL
+   public void IrParaCanal(int numero){
+    if(numero>= CanalMinimo && numero <= CanalMaximo)
+    {
+        Canal = numero;
+        UltimoCanal = numero;
+        Console.WriteLine($"Canal alterado para {Canal}");
+    }
+    else
+    {
+        Console.Writeline("Número de canal inválido");
+    }
+}
+
+    public void C
+
+
+
 }
 
