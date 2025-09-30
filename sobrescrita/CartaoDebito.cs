176801dbcb91f122a3dbedc80b3c85f1823af751
cvzeraa <165086449+cvzeraa@users.noreply.github.com>
2025-09-30 00:30:09 +0000

    feat: sobrescrita

---
+++ sobrescrita/CartaoDebito.cs
@@ -0,0 +1,8 @@
+public class CartaoDebito : Cartao
+{
+    public override void Debitar(decimal valor)
+    {
+        
+        Console.WriteLine($"Compra {valor:C} realizada no cartão de débito.");
+    }
+}
